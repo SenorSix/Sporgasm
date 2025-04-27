8c1f3f811d02dc9c0b32b9553969a236a54abf53
Mr 6 <Mr_6@sporgasm.org>
2025-04-27 00:04:37 -0400

    v7: updated gallery, cleaned JSON files, polished index page

--- app/index/index.py
+++ app/index/index.py
@@ -18,7 +18,7 @@ def home():
 
     # Pass filtered data to the template
     title_image = [item for item in pizza if item["title"] == "Title"]
-    link_images = [item for item in pizza if item["title"] in ["Yellow Mushroom", "Mushroom Edge"]]
+    link_images = [item for item in pizza if item["title"] in ["Yellow Mushroom", "Sporgasm Forum", "Mushroom Edge"]]
     gills_image = [item for item in pizza if item["title"] == "Gills"]
     return render_template(
         'index.html', 
