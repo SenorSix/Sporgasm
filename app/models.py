06803204e28a4715d83e0cac1b32f350e5e1d1b5
Mr 6 <Mr_6@sporgasm.org>
2025-06-07 15:30:05 -0400

    Add other_common_names field and integrate Alembic for DB migrations
    - Made other_common_names field work properly on spore_sheet.html
    - Implemented Alembic for cleaner database versioning
    - Updated CSS for spore_sheet.html and index.html

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from .database import Base
 from sqlalchemy import Column, Integer, String, Boolean, Text
+from sqlalchemy.dialects.postgresql import ARRAY
 
 
 class Mushroom(Base):
@@ -8,6 +9,7 @@ class Mushroom(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
+    other_common_names = Column(ARRAY(String), nullable=True)
     scientific_name = Column(String)
     description = Column(String)
     home_photo_path = Column(String)
